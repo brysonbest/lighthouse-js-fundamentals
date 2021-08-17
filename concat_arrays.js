3f7c96746f58b5a5401de07654d7d31cf878a66e
Bryson Best <brycekalden@gmail.com>
2021-08-17 19:37:18 +0000

    Initialized function that concatenates two arrays into one.

---
+++ concat_arrays.js
@@ -0,0 +1,16 @@
+//concatenates two arrays into one
+const concat = function(array1, array2){
+  let newArray = [];
+  for(let array of array1){
+    newArray.push(array);
+  }
+  for(let array of array2){
+    newArray.push(array);
+  }
+  return newArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
